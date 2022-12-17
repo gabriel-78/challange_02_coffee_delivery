6e44634ca363290f2ed0d900afe3859c704ec7c8
gabriel-78 <gabriel.78.rodrigues@gmail.com>
2022-12-16 23:54:13 -0300

    CONFIG: Colors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,4 +1,38 @@
+const baseLight = {
+  background: '#FAFAFA',
+  button: '#E6E5E5',
+  card: '#F3F2F2',
+  hover: '#D7D5D5',
+  input: '#EDEDED',
+  label: '#8D8686',
+  subTitle: '#403937',
+  text: '#574F4D',
+  title: '#272221',
+  white: '#FFFFFF',
+}
+
+const purple = {
+  dark: '#4B2995',
+  default: '#8047f8',
+  light: '#EBE5F9',
+}
+
+const yellow = {
+  dark: '#C47F17',
+  default: '#DBAC2C',
+  light: '#F1E9C9',
+}
+
+const product = {
+  purple,
+  yellow,
+}
+
+const lightTheme = {
+  base: baseLight,
+  product,
+}
+
 export const defaulTheme = {
-  primary: 'purple',
-  secondary: 'orange',
+  primary: lightTheme,
 }
