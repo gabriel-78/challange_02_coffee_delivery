6b72cabc6a5a9584bb5a1285b363f2f4d9e20ccc
gabriel-78 <gabriel.78.rodrigues@gmail.com>
2022-12-16 22:36:23 -0300

    CONFIG: Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
 import { defaulTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaulTheme}>
+      <GlobalStyle/>
       <div><h1>hello world</h1></div>
     </ThemeProvider>
   )
